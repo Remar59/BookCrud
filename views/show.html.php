f7cfd9613d691900a92dcedd3ab52ce3f129359e
Remar59 <remlevy@hotmail.fr>
2023-11-14 16:55:56 +0100

    dernier commit du jour

--- views/show.html.php
+++ views/show.html.php
@@ -1,9 +1,42 @@
 <?php require 'partials/header.html.php'; ?>
 
-<div class="max-w-5xl mx-auto">
-    <h1 class="text-2xl">
-        <?= $book['title']; dump($book) ?>
-    </h1>
-</div>
+<div class="max-w-5xl mx-auto px-3">
+        <div class="lg:flex items-center">
+            <div class="lg:w-1/2">
+                <img class="rounded-lg max-w-full mx-auto mb-12" src="/<?= $book['image']; ?>" alt="<?= $title['title']; ?>">
+            </div>
+            <div class="lg:w-1/2">
+                <h1 class="text-center text-2xl font-bold"><?= $title['title']; ?></h1>
+
+                <div class="flex items-center justify-between my-10">
+                    <div>
+                        <p class="text-4xl font-bold"><?= price($price, $discount); ?> €</p>
+                        <?php if ($discount > 0) { ?>
+                        <p class="text-lg font-bold">-<?= $discount; ?>% <span class="line-through"><?= price($price); ?> €</span></p>
+                        <?php } ?>
+                    </div>
+                    <div class="text-lg text-gray-900">
+                        <p>
+                            Par <strong><?= $author; ?></strong>
+                        </p>
+                        <p>
+                            <?php // strtotime('1991-11-18'); ?>
+                            Publié le <?= date('d/m/Y', strtotime($publishedAt)); ?>
+                        </p>
+                    </div>
+                </div>
+
+                <p class="text-xl text-center text-gray-900">
+                    ISBN: <strong><?= isbn($isbn); ?></strong>
+                </p>
+
+                <div class="text-center mt-12">
+                    <a class="bg-gray-900 px-4 py-2 text-white inline-block rounded hover:bg-gray-700 duration-200" href="/cart/1/add">
+                        Ajouter au panier
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php require 'partials/footer.html.php'; ?>
